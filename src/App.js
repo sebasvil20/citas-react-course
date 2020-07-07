1112d6bde642fc650769dbf5394308246eda36fe
Hinval <sebasvil20@gmail.com>
2020-07-07 10:16:15 -0500

    initial commit
    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Fragment, useState } from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
+
+function App() {
+
+  //Arreglo de citas
+  const [citas, guardarCitas] = useState([]);
+
+  //Funcion que tome las citas actuales y agregue la nueva
+  const crearCita = cita =>{
+    guardarCitas({ ...citas, cita });
+  };
+
+  return (
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='one-half column'>
+            <Formulario 
+              crearCita={crearCita}
+            />
+          </div>
+          <div className='one-half column'>
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita 
+                key={cita.id}
+                cita={cita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
